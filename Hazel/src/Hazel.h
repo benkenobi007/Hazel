0af382b0fa6becc411833461acb5d4452e2eeb1c
benkenobi007 <ktiwari998@gmail.com>
2020-05-13 03:04:08 +0530

    logging module

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,6 +3,7 @@
 // FOR USE BY HAZEL APPLICATIONS
 
 #include"Hazel/Application.h"
+#include"Hazel/Log.h"
 
 // ----Entry Point ---------------
 #include "Hazel/EntryPoint.h"
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,10 @@ extern Hazel::Application* Hazel::createApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::init();
+	HZ_CORE_WARN("Log for engine");
+	int a = 10;
+	HZ_INFO("Log for app. Var={0}", a);
 	auto app = Hazel::createApplication();
 	app->Run();
 	delete app;
